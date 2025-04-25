141c009dca3c7f1334b3f99fdc12d2b0f90525df
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-04-25 20:18:23 +0100

    Create src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+src/
+├── assets/
+│   └── sounds/
+│       ├── increase.mp3
+│       ├── decrease.mp3
+│       └── reset.mp3
+├── components/
+│   ├── ActionButton.jsx
+│   └── CounterDisplay.jsx
+├── hooks/
+│   └── usePersistentState.js
+├── App.jsx
+└── main.jsx
